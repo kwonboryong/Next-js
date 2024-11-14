4abfb3309abf294f0705b0a345e4ab971ccb7026
kwonboryong <kwonboryong@naver.com>
2024-11-14 19:48:55 +0900

    Fix: 빌드 에러로 인한 타입스크립트 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,21 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string };
-}
+type PageParams = {
+  params: {
+    id: string;
+  };
+};
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
+export async function generateMetadata({ params }: PageParams) {
   const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+export default async function MovieDetailPage({ params }: PageParams) {
+  const { id } = params;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>

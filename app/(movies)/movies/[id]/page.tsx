23542e573ad3ec0095e0f8f207b55280314bd844
kwonboryong <kwonboryong@naver.com>
2024-11-14 19:51:42 +0900

    Fix: 빌드 에러로 인한 수정

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,21 +2,20 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-type PageParams = {
-  params: {
-    id: string;
-  };
-};
+interface IParams {
+  params: { id: string };
+}
 
-export async function generateMetadata({ params }: PageParams) {
+export async function generateMetadata({ params }: IParams) {
   const movie = await getMovie(params.id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params }: PageParams) {
+export default async function MovieDetailPage({ params }: IParams) {
   const { id } = params;
+
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
